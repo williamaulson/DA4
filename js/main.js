637066c091581472b1ffd7374aa8700d7e04b70d
williamaulson <waulson@gmu.edu>
2015-02-26 11:30:45 -0500

    v64

--- js/main.js
+++ js/main.js
@@ -118,8 +118,8 @@ window.onload = function()
     //var p1Text = 'Patient 1\nAge: 24\nWorks Retail\nNo Family\nHIV Positive';
     //var p2Text = 'Patient 2\nAge: 52\nUnemployed\nThree Children\nConvicted Rapist';
         
-    var p1text1 = ['Patient 1\nAge: 25\nProstitute', 'Patient 1\nAge: 25\nProstitute\nMethamphetamine\nAddict', 'Patient 1\nAge: 25\nProstitute\nMethamphetamine\nAddict\nAttempted Suicide\nMultiple Times'];
-    var p2text1 = ['Patient 2\nAge: 38\nExecutive', 'Patient 2\nAge: 38\nExecutive\nServed Time for\nEmbezzlement', 'Patient 2\nAge: 38\nExecutive\nServed Time for\nEmbezzlement\nDonates to\nHate Groups'];
+    var p1text1 = ['Patient 1\nAge: 54\nEducator', 'Patient 1\nAge: 54\nEducator\nCancer Survivor', 'Patient 1\nAge: 54\nEducator\nCancer Survivor\nRecently Divorced'];
+    var p2text1 = ['Patient 2\nAge: 51\nCustodian', 'Patient 2\nAge: 51\nCustodian\nDiabetic', 'Patient 2\nAge: 51\nCustodian\nDiabetic\nLearning Disabled'];
     var p1text2 = ['Patient 1\nAge: 25\nProstitute', 'Patient 1\nAge: 25\nProstitute\nMethamphetamine\nAddict', 'Patient 1\nAge: 25\nProstitute\nMethamphetamine\nAddict\nAttempted Suicide\nMultiple Times'];
     var p1text3 = ['Patient 1\nAge: 15\nWard of the State', 'Patient 1\nAge: 15\nWard of the State\nMathematical Genius', 'Patient 1\nAge: 15\nWard of the State\nMathematical Genius\nMurdered Family\nWhen Age 12'];
     var p1text4 = ['Patient 1\nAge: 33\nDay Laborer', 'Patient 1\nAge: 33\nDay Laborer\nConvicted Felon', 'Patient 1\nAge: 33\nDay Laborer\nConvicted Felon\nAvid Knitter'];
@@ -167,7 +167,7 @@ window.onload = function()
     	    text1 = game.add.text(75, 75, textArray1[0][0], textStyle);
     	    text2 = game.add.text(780, 75, textArray2[0][0], textStyle);
     	    //timeText = game.add.text(490, 75, 'Time Left: ' + (game.time.now - startTime), textTimeStyle);
-    	    timeText = game.add.text(450, 75, 'ZTime Left: ' + Math.floor(((15999 - (game.time.now - startTime)) / 1000) % 60), textTimeStyle);
+    	    timeText = game.add.text(450, 75, 'ATime Left: ' + Math.floor(((15999 - (game.time.now - startTime)) / 1000) % 60), textTimeStyle);
     	        	    
     	    cursors = game.input.keyboard.createCursorKeys();
     	    spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
@@ -828,7 +828,7 @@ window.onload = function()
     	    var tx3 = 'For your ' + choice[2] + ' choice you ' + choiceText[choiceArray[2]] + '.\n\n';
     	    var tx4 = 'For your ' + choice[3] + ' choice you ' + choiceText[choiceArray[3]] + '.\n\n';
     	    var tx5 = 'For your ' + choice[4] + ' choice you ' + choiceText[choiceArray[4]] + '.\n\n';
-    	    var endtext = game.add.text(110, 35, tx1 + tx2 + tx3 + tx4 + tx5 + 'Did more information make your decision easier or harder?\n\nDid the words used create patient preconceptions?', textStyle);
+    	    var endtext = game.add.text(110, 35, tx1 + tx2 + tx3 + tx4 + tx5 + 'Did more information make your decision easier or harder?\n\nDid the words used cause preconceptions about the patients?', textStyle);
     }
     
     
